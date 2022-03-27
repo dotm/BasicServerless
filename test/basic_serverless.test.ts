1e70c8bc0d6967df2ab3222286ee4ded892bae73
dotm <desde.el.mar96@gmail.com>
2022-03-27 17:00:38 +0700

    init commit

---
+++ test/basic_serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BasicServerless from '../lib/basic_serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/basic_serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BasicServerless.BasicServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
